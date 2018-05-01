656eed0cbfbeed2814b809b586eacb6fc0c4c11e
uyuyuuy <842276675@qq.com>
2018-05-02 00:54:11 +0800

    V2.0.1
    客户端：
    图片上传
    添加商品

---
+++ client/pages/goods/addGoods.js
@@ -0,0 +1,152 @@
+// pages/addGoods/addGoods.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  setGoodsName: function (e) {
+    console.log(e);
+    this.setData({
+      goodsName: e.detail.value
+    })
+  },
+
+  setGoodsDescription: function (e) {
+    console.log(e);
+    this.setData({
+      goodsDescription: e.detail.value
+    })
+  },
+
+  // 上传图片接口
+  doUpload: function () {
+    var that = this
+
+    // 选择图片
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        util.showBusy('正在上传')
+        var filePath = res.tempFilePaths[0]
+
+        // 上传图片
+        wx.uploadFile({
+          url: config.service.uploadUrl,
+          filePath: filePath,
+          name: 'file',
+
+          success: function (res) {
+            util.showSuccess('上传图片成功')
+            console.log(res)
+            res = JSON.parse(res.data)
+            console.log(res)
+            that.setData({
+              goodsImgUrl: res.data.imgUrl
+            })
+          },
+
+          fail: function (e) {
+            util.showModel('上传图片失败')
+          }
+        })
+
+      },
+      fail: function (e) {
+        console.error(e)
+      }
+    })
+  },
+
+  // 预览图片
+  previewImg: function () {
+    wx.previewImage({
+      current: this.data.goodsImgUrl,
+      urls: [this.data.goodsImgUrl]
+    })
+  },
+
+  addGoods: function () {
+    wx.request({
+      url: 'https://qmuao5e8.qcloud.la/weapp/goods/add_goods', //仅为示例，并非真实的接口地址
+      data: {
+        name: this.data.goodsName,
+        description: this.data.goodsDescription,
+        goods_img: this.data.goodsImg
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        util.showSuccess('上传图片成功')
+      }
+    })
+
+  }
+
+
+})
--- server/lib/db.js
+++ server/lib/db.js
@@ -1,6 +1,3 @@
-const mysql = require('mysql');
-
-
 const Sequelize = require('sequelize');
 
 const config = require('../config');
